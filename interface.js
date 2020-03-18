33cc0f9912c5ea594a285d4c98daaa671cfca942
Patrick Oliver <poliver24@hotmail.com>
2020-03-18 18:44:12 +0000

    temperature text colored; API added for current temperature

--- interface.js
+++ interface.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
+  displayWeather('London');
+
   updateTemperature();
 
   // This reads and updates the new temperature every time its modified
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage())
   };
 
   $('#temperature-up').click(function() {
@@ -33,4 +36,26 @@ $(document).ready(function() {
     $('#power-saving-status').text('off');
   });
 
+
+  // $('#current-city').change(function(){
+  //   var city = $('#current-city').val();
+
+  //    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
+  
+  //     $('#current-temperature').text(data.main.temp);
+  //   });
+
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function (data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  };
 })
